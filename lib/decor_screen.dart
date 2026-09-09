fdf67a92ebe09183f75d204b4cfedbec9ed0a15d
martin198799-design <martin198799@gmail.com>
2026-09-10 01:24:43 +0300

    Update decor_screen.dart

--- lib/decor_screen.dart
+++ lib/decor_screen.dart
@@ -7,8 +7,7 @@ class DecorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SectionScreenTemplate(
-      sectionKey: 'decor',
-      defaultTitle: 'قسم الديكور المسرحي',
+      sectionTitle: 'قسم الديكور المسرحي',
     );
   }
 }
