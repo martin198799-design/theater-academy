106e1d71d8cebf42a47f30e1b2eb5853ba96ecd4
martin198799-design <martin198799@gmail.com>
2026-09-09 15:40:27 +0300

    Create theater_models.dart

---
+++ lib/theater_models.dart
@@ -0,0 +1,12 @@
+// نماذج البيانات الأساسية للمسرح
+class PlayItem {
+  final String title;
+  final String details;
+  PlayItem({required this.title, required this.details});
+}
+
+class StudyItem {
+  final String title;
+  final String content;
+  StudyItem({required this.title, required this.content});
+}
